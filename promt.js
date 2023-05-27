6d18feebc16b8da233574a0871a89ee3f778b210
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-05-27 15:30:57 +0600

    alart done

---
+++ promt.js
@@ -0,0 +1,11 @@
+console.log('showAlart');
+// alert('ki khobor');
+const ashowAlart = () =>
+{
+    const num = Math.random()*10;
+    console.log(num);
+    if(num < 5)
+    {
+        alert('ki khobor dst');
+    }
+}
